3cb46e54461c54e2f8272ed0846ee143b905f3e8
Jorge Teixeira <jorgesergio.teixeira@gmail.com>
2020-06-17 20:11:48 -0300

    Inclusão dos exercícios no arquivo variaveis.js.

---
+++ JavaScript para Iniciantes/variaveis.js
@@ -0,0 +1,60 @@
+var nome = 'Sérgio'
+let idade = 26
+const possuiFaculdade = false
+
+var preco = 20
+var totalComprado = 5
+var totalPreco = preco * totalComprado
+
+console.log(totalPreco)
+
+var sobrenome = 'Teixeira',
+  cidade = 'Cabo Frio',
+  estado = 'Rio'
+
+console.log(sobrenome, cidade, estado)
+
+var precoAplicativo
+console.log(precoAplicativo)
+
+var igreja = 'IPB'
+console.log(igreja)
+igreja = 'Batista'
+console.log(igreja)
+
+let vertente = 'Calvinista'
+console.log(vertente)
+vertente = 'Arminiano'
+console.log(vertente)
+
+const confissao = 'CFW'
+console.log(confissao)
+// confissao = 'Batista 1969'
+// console.log(confissao)
+
+// EXERCICIO
+// Declarar uma variavel com o seu nome
+var nome1 = 'Jorge'
+console.log(nome1)
+
+// Declarar uma variável com a sua idade
+var idade1 = 25
+console.log(idade1)
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comida
+console.log(comida)
+
+// Atribuir valor a sua comida favorita
+comida = 'Bife e batata frita'
+console.log(comida)
+
+
+// Declarar 5 variáveis diferentes sem valores
+var carro,
+  moto,
+  celular,
+  computador,
+  marca
+console.log(carro, moto, celular, computador, marca)
