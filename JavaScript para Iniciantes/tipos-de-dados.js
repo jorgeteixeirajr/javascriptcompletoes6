3e80ab10b5648b51d53cec8af4b6ac7407c76712
Jorge Teixeira <jorgesergio.teixeira@gmail.com>
2020-06-17 20:25:48 -0300

    Inclusão dos exercícios no arquivo tipos-de-dados.js.

---
+++ JavaScript para Iniciantes/tipos-de-dados.js
@@ -0,0 +1,41 @@
+// String
+var nome = 'Jorge';
+// Number
+var idade = 25;
+// Boolean
+var possuiFaculdade = false;
+// Undefined
+var time;
+// Null
+var comida = null;
+// Symbol
+var simbolo = Symbol()
+// Object
+var novoObjeto = {}
+
+//verifica o tipo de  variável
+console.log(typeof nome)
+
+
+// EXERCÍCIO
+// Declare uma variável contendo uma string
+var igreja = 'IPB'
+
+// Declare uma variável contendo um número dentro de uma string
+var confissao = '1646'
+
+// Declare uma variável com a sua idade
+var idade1 = 25
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var primeiroNome = 'Jorge',
+  ultimoNome = 'Teixeira'
+console.log(`${primeiroNome} ${ultimoNome}`)
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = `It's time`
+console.log(frase)
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome)
