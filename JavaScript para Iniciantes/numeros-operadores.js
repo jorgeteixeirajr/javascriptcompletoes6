19fe141819aeb85df78088afe5a890d020ea0148
Jorge Teixeira <jorgesergio.teixeira@gmail.com>
2020-06-17 21:24:34 -0300

    Inclusão dos exercícios no arquivo numeros-operadores.js.

---
+++ JavaScript para Iniciantes/numeros-operadores.js
@@ -0,0 +1,30 @@
+// EXERCÍCIO
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total)
+
+// Crie duas expressões que retornem NaN
+var nome = 'Jorge'
+var idade = 25
+console.log(nome / idade)
+
+var igreja = 'IPB'
+var confissao = 1646
+console.log(confissao * igreja)
+
+// Somar a string '200' com o número 50 e retornar 250
+var solicitacao = '200'
+var numero = 50
+console.log(+solicitacao + numero)
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var incremento = 5
+console.log(incremento++)
+console.log(incremento)
+
+// Como dividir o peso por 2?
+var numero = +'80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade;
+
+console.log(peso)
