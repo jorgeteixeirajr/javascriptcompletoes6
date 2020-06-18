14d4a58aac83b348dae6d3258355edf7455016f9
Jorge Teixeira <jorgesergio.teixeira@gmail.com>
2020-06-18 15:42:10 -0300

    Inclusão do arquivo index.html e exercícios funcoes.js.

--- JavaScript para Iniciantes/funcoes.js
+++ JavaScript para Iniciantes/funcoes.js
@@ -37,3 +37,69 @@ function terceiraIdade(idade) {
   }
 }
 console.log(terceiraIdade(60))
+
+// EXERCÍCIO
+
+// Crie uma função para verificar se um valor é Truthy
+function verdadeiro(teste) {
+  if (teste === 'teste') {
+    return true
+  } else {
+    return false
+  }
+}
+console.log(verdadeiro('teste'))
+
+// Crie uma função matemática que retorne o perímetro de um quadrado
+// lembrando: perímetro é a soma dos quatro lados do quadrado
+function perimetroQuadrado(lado) {
+  return lado * 4
+}
+console.log(perimetroQuadrado(10))
+
+// Crie uma função que retorne o seu nome completo
+// ela deve possuir os parâmetros: nome e sobrenome
+function nomeCompleto(nome, sobrenome) {
+  return `${nome} ${sobrenome}`
+}
+console.log(nomeCompleto('Jorge', 'Teixeira'))
+
+// Crie uma função que verifica se um número é par
+function ePar(numero) {
+  var modulo = numero % 2
+  if (modulo === 0) {
+    return true
+  } else {
+    return false
+  }
+}
+console.log(ePar(2))
+
+// Crie uma função que retorne o tipo de
+// dado do argumento passado nela (typeof)
+function argumento(origamid) {
+  return (typeof origamid)
+}
+console.log(argumento('origamid'))
+
+// addEventListener é uma função nativa do JavaScript
+// o primeiro parâmetro é o evento que ocorre e o segundo o Callback
+// utilize essa função para mostrar no console o seu nome completo
+// quando o evento 'click' ocorrer.
+addEventListener('click', function () {
+  console.log(`Jorge Teixeira`)
+})
+
+
+// Corrija o erro abaixo
+var totalPaises = 193;
+
+function precisoVisitar(paisesVisitados) {
+  return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+}
+
+function jaVisitei(paisesVisitados) {
+  return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+}
+precisoVisitar(20);
+jaVisitei(20);
