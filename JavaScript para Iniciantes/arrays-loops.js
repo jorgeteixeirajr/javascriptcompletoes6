112ecc611dad3b413297a864ca1386351da12f52
Jorge Teixeira <jorgesergio.teixeira@gmail.com>
2020-06-19 20:15:21 -0300

    Inclusão dos exercícios no arquivo arrays-loops.js.

--- JavaScript para Iniciantes/arrays-loops.js
+++ JavaScript para Iniciantes/arrays-loops.js
@@ -47,3 +47,5 @@ for (var i = 0; i < frutas.length; i++) {
 
 // Coloque a última fruta da array acima em uma variável,
 // sem remover a mesma da array.
+var ultimaFruta = frutas[4];
+console.log(ultimaFruta);
