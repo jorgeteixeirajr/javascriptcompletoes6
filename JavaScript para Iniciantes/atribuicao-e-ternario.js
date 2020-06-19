369a3debd6134531a49bb09cd5a5b2bfe8bf2cb6
Jorge Teixeira <jorgesergio.teixeira@gmail.com>
2020-06-19 20:58:48 -0300

    Inclusão dos exercícios no arquivo atribuicao-e-ternario.js.

--- JavaScript para Iniciantes/atribuicao-e-ternario.js
+++ JavaScript para Iniciantes/atribuicao-e-ternario.js
@@ -1,9 +1,9 @@
 // Some 500 ao valor de scroll abaixo,
 // atribuindo o novo valor a scroll
 var scroll = 1000;
-addEventListener('scroll', function () {
-  console.log(scroll + 500);
-})
+scroll += 500
+console.log(scroll)
+
 
 // Atribua true para a variável darCredito,
 // caso o cliente possua carro e casa.
@@ -11,5 +11,5 @@ addEventListener('scroll', function () {
 var possuiCarro = true;
 var possuiCasa = true;
 var darCredito;
-darCredito = (possuiCarro === true && possuiCasa === true) ? true : false
+darCredito = (possuiCarro && possuiCasa)
 console.log(darCredito)
