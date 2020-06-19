60c9859ab5c746679aa488f37a388e6622b79b7c
Jorge Teixeira <jorgesergio.teixeira@gmail.com>
2020-06-18 22:01:31 -0300

    Inclusão da tag button no index.html e  dos exercícios no arquivo tudo-e-objeto.js

---
+++ JavaScript para Iniciantes/tudo-e-objeto.js
@@ -0,0 +1,28 @@
+var nome = 'Jorge'
+
+var nomeMaiusculo = nome.toUpperCase()
+console.log(nomeMaiusculo)
+var nomeMinusculo = nome.toLowerCase()
+console.log(nomeMinusculo)
+
+var numero = 1.8
+numero.toString()
+numero.toFixed()
+
+// EXERCÍCIO
+// nomeie 3 propriedades ou métodos de strings
+var name = 'Sérgio'
+// fixed
+// length
+// toUpperCase
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector('.btn')
+// classList
+// addEventListener
+// getAttribute
+// text - content
+// id
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
