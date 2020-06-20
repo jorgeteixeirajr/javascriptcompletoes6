d15e9820f44dde9793dc06c4fa8bf802946fa3e3
Jorge Teixeira <jorgesergio.teixeira@gmail.com>
2020-06-20 17:54:56 -0300

    Ajuste nos arquivos index.html e o-que-e-dom.js.

--- DOM para Iniciantes/o-que-e-dom.js
+++ DOM para Iniciantes/o-que-e-dom.js
@@ -1,1 +1,12 @@
-console.log('teste')
+const href = window.location.href
+
+console.log(href)
+
+if (href === 'http://127.0.0.1:5500/DOM%20para%20Iniciantes/index.html') {
+  console.log('É igual')
+} else {
+  console.log('Nada haver')
+}
+
+const h1Selecionado = document.querySelector('h1')
+console.log(h1Selecionado)
