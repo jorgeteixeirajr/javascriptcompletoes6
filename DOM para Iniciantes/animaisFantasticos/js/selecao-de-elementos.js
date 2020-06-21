4c7991de6f48775e0b8b1190a9a8a47ffef3880d
Jorge Teixeira <jorgesergio.teixeira@gmail.com>
2020-06-20 22:03:32 -0300

    Inclusão dos exercícios no arquivo selecao-de-elementos.js.

---
+++ DOM para Iniciantes/animaisFantasticos/js/selecao-de-elementos.js
@@ -0,0 +1,20 @@
+// EXERCÍCIO
+// Retorne no console todas as imagens do site
+const imagensSite = document.querySelectorAll('.grid-section img')
+console.log(imagensSite)
+
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+const imagem = document.querySelectorAll('img[src^="assets/imagem"]')
+console.log(imagem)
+
+// Selecione todos os links internos (onde o href começa com #)
+const linkInterno = document.querySelectorAll('[href^="#"')
+console.log(linkInterno)
+
+// Selecione o primeiro h2 dentro de .animais-descricao
+const segundoTitulo = document.querySelector('.animais-descricao h2:first-child')
+console.log(segundoTitulo)
+
+// Selecione o último p do site
+const ultimoParagrafo = document.querySelectorAll('p:last-child')
+console.log(ultimoParagrafo)
